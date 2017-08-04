db1624210bd92894a51e6a7147c202e225066bcd
Ellie Harper <EllieFops@users.noreply.github.com>
2017-08-03 23:20:19 -0400

    I AM MAYOR

--- I AM MAYOR.java
+++ I AM MAYOR.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
   public static void main(String []args){
     boolean I_AM_MAYOR = true;
-    while(I_AM_MAYOR = true)
+    while(I_AM_MAYOR)
       System.out.println("I AM MAYOR");
   }
 }
